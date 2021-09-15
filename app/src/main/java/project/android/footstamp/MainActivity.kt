7a6902e26005cd1516abaa2626e8e0e424cbb69c
SnoopyKim <snoopykim0@gmail.com>
2021-09-15 13:31:52 +0900

    Fix gradle setting & change menu id

--- app/src/main/java/project/android/footstamp/MainActivity.kt
+++ app/src/main/java/project/android/footstamp/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment
 import project.android.footstamp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
     private lateinit var mapFragment: Fragment
     private lateinit var galleryFragment: Fragment
@@ -27,30 +26,23 @@ class MainActivity : AppCompatActivity() {
 
         with(binding) {
             bottomNavigationView.setOnItemSelectedListener {
-                val fragmentTransaction = supportFragmentManager.beginTransaction()
-                val fragment = when(it.itemId) {
-                    R.id.first -> mapFragment
-                    R.id.second -> galleryFragment
-                    R.id.third -> settingFragment
-                    else -> mapFragment
-                }
-                fragmentTransaction.replace(R.id.fragment_container, fragment)
-                fragmentTransaction.addToBackStack(null)
-                fragmentTransaction.commit()
+                switchFragment(it.itemId)
                 Toast.makeText(applicationContext, it.title, Toast.LENGTH_SHORT).show()
                 true
             }
         }
     }
 
-//    private fun switchFragment(id: Int) {
-//        val fragment = when(id) {
-//            R.id.first -> mapFragment
-//            R.id.second -> galleryFragment
-//            R.id.third -> settingFragment
-//            else -> mapFragment
-//        }
-//        fragmentTransaction.replace(R.id.main_fragment, fragment)
-//        fragmentTransaction.commit()
-//    }
+    private fun switchFragment(id: Int) {
+        val fragment = when(id) {
+            R.id.map -> mapFragment
+            R.id.gallery -> galleryFragment
+            R.id.setting -> settingFragment
+            else -> mapFragment
+        }
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
 }
