5bf23dbdeceab118e92b1e1478cdceafd7f46a56
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-09-19 18:28:12 +0900

    리사이클러 뷰 구현 및 날짜샘플 생성

--- app/src/main/java/project/android/footstamp/adapter/GalleryVIewAdapter.kt
+++ app/src/main/java/project/android/footstamp/adapter/GalleryVIewAdapter.kt
@@ -1,3 +1,35 @@
 package project.android.footstamp.adapter
 
-public class GalleryVIewAdapter {}
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import project.android.footstamp.R
+
+public class GalleryVIewAdapter(val items: MutableList<String>) : RecyclerView.Adapter<GalleryVIewAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryVIewAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_stamp_list,parent,false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: GalleryVIewAdapter.ViewHolder, position: Int) {
+        holder.bindItems(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class ViewHolder(itemView : View): RecyclerView.ViewHolder(itemView){
+        fun bindItems(item : String){
+
+            val Gallery_text = itemView.findViewById<TextView>(R.id.ItemStampText)
+            Gallery_text.text = item
+
+        }
+    }
+}
--- app/src/main/java/project/android/footstamp/fragment/GalleryFragment.kt
+++ app/src/main/java/project/android/footstamp/fragment/GalleryFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import project.android.footstamp.R
+import project.android.footstamp.adapter.GalleryVIewAdapter
 
 class GalleryFragment : Fragment() {
 
@@ -17,25 +20,28 @@ class GalleryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_gallery, container, false)
+
+        val items = mutableListOf<String>()
+        items.add("날짜 샘플1")
+        items.add("날짜 샘플2")
+        items.add("날짜 샘플3")
+        items.add("날짜 샘플4")
+
+        val rv = view.findViewById<RecyclerView>(R.id.GalleryRCView)
+        val rvAdapter = GalleryVIewAdapter(items)
+
+        rv.adapter = rvAdapter
+        rv.layoutManager = LinearLayoutManager(context)
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MapFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
         fun newInstance(param1: String, param2: String) =
             GalleryFragment().apply {
                 // TODO: 일단 냅둬
-
             }
-    }
 }
