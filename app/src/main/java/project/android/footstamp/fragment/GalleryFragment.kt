0a6bc5d9e068a93936e3fd7a5a37ac2a280b671b
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-10-02 22:10:24 +0900

    탭 수정 및 오류 주석화

--- app/src/main/java/project/android/footstamp/fragment/GalleryFragment.kt
+++ app/src/main/java/project/android/footstamp/fragment/GalleryFragment.kt
@@ -1,37 +1,20 @@
 package project.android.footstamp.fragment
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
-import androidx.constraintlayout.helper.widget.Carousel
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayout.TabLayoutOnPageChangeListener
 import com.google.android.material.tabs.TabLayoutMediator
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import project.android.footstamp.R
 import project.android.footstamp.R.id.pager
-import project.android.footstamp.activity.SplashActivity
-import project.android.footstamp.adapter.GalleryViewAdapter
 import project.android.footstamp.adapter.PagerAdapter
-import project.android.footstamp.model.Stamp
-import project.android.footstamp.viewmodel.StampViewModel
-import java.util.*
 
 class GalleryFragment : Fragment() {
-    var items = mutableListOf<Stamp>()
-    lateinit var galadapter : GalleryViewAdapter
-    lateinit var PagerAdapter : PagerAdapter
+
+    lateinit var viewPager2: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,46 +28,13 @@ class GalleryFragment : Fragment() {
 
         // Inflate the layout for this fragment
 
-        val view = inflater.inflate(R.layout.fragment_view, container, false)
-        val rv = view.findViewById<RecyclerView>(R.id.GalleryRCView)
-        galadapter = GalleryViewAdapter(items)
+        val view = inflater.inflate(R.layout.fragment_gallery, container, false)
 
-        rv.adapter = galadapter
-        rv.layoutManager = GridLayoutManager(context,3)
-
-            galadapter.itemClick = object : GalleryViewAdapter.ItemClick{
-            override fun onClick(view: View, Position: Int) {
-                val intent = Intent(context, ViewFragment::class.java)
-                startActivity(intent)
-            }
-
-        }
-        val TabLayout = view.findViewById<TabLayout>(R.id.GalleryTab)
-        val ViewPager = view.findViewById<ViewPager2>(pager)
-        TabLayoutMediator(TabLayout,ViewPager){tab, position ->
-            tab.text = "지역별 보기"
-        }.attach();
-
-
+        val ViewPager2 = view.findViewById<ViewPager2>(pager)
+//        ViewPager2.adapter = PagerAdapter(GalleryFragment())
 
         return view
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val stampViewModel = StampViewModel(requireActivity().application)
-        CoroutineScope(Dispatchers.Main).launch {
-            stampViewModel.getAll().observe(viewLifecycleOwner, { stamps ->
-             galadapter.setList(stamps)
-            })
-        }
-        val addBtn = view.findViewById<FloatingActionButton>(R.id.addBtn)
-        addBtn.setOnClickListener {
-            val intent = Intent(context, SplashActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
 
         fun newInstance(param1: String, param2: String) =
             GalleryFragment().apply {
--- app/src/main/java/project/android/footstamp/fragment/ViewFragment.kt
+++ app/src/main/java/project/android/footstamp/fragment/ViewFragment.kt
@@ -1,14 +1,27 @@
 package project.android.footstamp.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import project.android.footstamp.R
+import project.android.footstamp.activity.SplashActivity
+import project.android.footstamp.adapter.GalleryViewAdapter
+import project.android.footstamp.model.Stamp
+import project.android.footstamp.viewmodel.StampViewModel
 
 class ViewFragment : Fragment() {
     // TODO: Rename and change types of parameters
+    var items = mutableListOf<Stamp>()
+    lateinit var galadapter : GalleryViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +33,39 @@ class ViewFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
+        val view = inflater.inflate(R.layout.fragment_view, container, false)
         // Inflate the layout for this fragment
+        val rv = view.findViewById<RecyclerView>(R.id.GalleryRCView)
 
-        return inflater.inflate(R.layout.fragment_view, container, false)
+        galadapter = GalleryViewAdapter(items)
+
+        rv.adapter = galadapter
+        rv.layoutManager = GridLayoutManager(context,3)
+
+        galadapter.itemClick = object : GalleryViewAdapter.ItemClick{
+            override fun onClick(view: View, Position: Int) {
+                val intent = Intent(context, ViewFragment::class.java)
+                startActivity(intent)
+            }
+
+        }
+        return view
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val stampViewModel = StampViewModel(requireActivity().application)
+        CoroutineScope(Dispatchers.Main).launch {
+            stampViewModel.getAll().observe(viewLifecycleOwner, { stamps ->
+                galadapter.setList(stamps)
+            })
+        }
+        val addBtn = view.findViewById<FloatingActionButton>(R.id.addBtn)
+        addBtn.setOnClickListener {
+            val intent = Intent(context, SplashActivity::class.java)
+            startActivity(intent)
+        }
     }
 
         fun newInstance(param1: String, param2: String) =
