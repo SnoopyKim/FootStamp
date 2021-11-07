00b5130e2eed55e1348d23883c73a3b6dae7d7b9
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-11-07 22:20:20 +0900

    갤러리 액티비티 생성

---
+++ app/src/main/java/project/android/footstamp/activity/GalleryActivity.kt
@@ -0,0 +1,12 @@
+package project.android.footstamp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import project.android.footstamp.R
+
+class GalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery)
+    }
+}
---
+++ app/src/main/java/project/android/footstamp/adapter/GalleryViewAdapter2.kt
@@ -0,0 +1,67 @@
+package project.android.footstamp.adapter
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.icu.text.Transliterator
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.contentValuesOf
+import androidx.core.os.persistableBundleOf
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import project.android.footstamp.R
+import project.android.footstamp.fragment.ViewFragment
+import project.android.footstamp.model.Stamp
+import project.android.footstamp.utils.PostModel
+
+class  GalleryViewAdapter2(val context : Context,
+                          val List : MutableList<PostModel>)
+    : RecyclerView.Adapter<GalleryViewAdapter2.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int, ): GalleryViewAdapter2.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_stamp_list,parent,false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: GalleryViewAdapter2.ViewHolder, position: Int) {
+        holder.bindItems(List[position])
+    }
+
+    override fun getItemCount(): Int {
+        return List.size
+    }
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        fun bindItems(item : PostModel){
+            val date = itemView.findViewById<TextView>(R.id.ItemStampText)
+            val memo = itemView.findViewById<TextView>(R.id.ItemStampMemo)
+            val image = itemView.findViewById<ImageView>(R.id.ItemStampImage)
+            val key = item.key
+            val storageReference = Firebase.storage.reference.child(key + ".png")
+
+            date!!.setText(List[position].time)
+            memo!!.setText(List[position].memo)
+
+
+            storageReference.downloadUrl.addOnCompleteListener(OnCompleteListener { task ->
+                if (task.isSuccessful) {
+
+                    Glide.with(context)
+                        .load(task.result)
+                        .into(image)
+                }
+            })
+
+
+            itemView.setOnClickListener{
+                Toast.makeText(context,"터치 확인",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
