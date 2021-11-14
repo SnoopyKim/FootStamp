9ad1aa87b612c906b39da463ae8992265b840123
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-11-14 22:57:32 +0900

    게시판 뷰 구현 및 오류 수정

--- app/src/main/java/project/android/footstamp/adapter/BoardAdapter.kt
+++ app/src/main/java/project/android/footstamp/adapter/BoardAdapter.kt
@@ -2,10 +2,18 @@ package project.android.footstamp.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import project.android.footstamp.R
+import project.android.footstamp.utils.FBAuth
 import project.android.footstamp.utils.PostModel
 
 class BoardAdapter(val context: Context, val List : MutableList<PostModel>,val uid : String)
@@ -24,6 +32,28 @@ class BoardAdapter(val context: Context, val List : MutableList<PostModel>,val u
     }
         inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
             fun bindItems(item:PostModel){
+                val date = itemView.findViewById<TextView>(R.id.dateArea)
+                val uid = itemView.findViewById<TextView>(R.id.invuid)
+                val area = itemView.findViewById<TextView>(R.id.itemview_area)
+                val image = itemView.findViewById<ImageView>(R.id.ItemViewImage)
+                val district = itemView.findViewById<TextView>(R.id.itemview_district)
+                val key = item.key
+                val memo = item.memo
+                val storageReference = Firebase.storage.reference.child(key + ".png")
+
+                storageReference.downloadUrl.addOnCompleteListener(OnCompleteListener { task ->
+                    if (task.isSuccessful) {
+
+                        Glide.with(context)
+                            .load(task.result)
+                            .into(image)
+                    }
+                })
+
+                date!!.setText(item.time)
+                area!!.setText(item.area)
+                district!!.setText(item.district)
+
 
             }
         }
